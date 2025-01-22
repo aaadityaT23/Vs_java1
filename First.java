53a6f0d8f89427f79a0654f12a8b673b901b742e
aditya <mayureshtiwalkar@gmail.com>
2025-01-22 22:21:07 +0530

    create arraylist

--- First.java
+++ First.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Demo {
     public static void main(String[] args) {
         System.out.println("welcome");
+
+        List<String> name = new ArrayList<>();
+        name.add("hitesh");
+        name.add("Smith");
+
     }
 }
